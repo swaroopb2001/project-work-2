6f37461c031ce132a18000b802a4aac49273ffd6
Sai Pranav <saipranav429@gmail.com>
2022-07-29 23:49:33 +0530

    intial frontend

---
+++ traffic_manager/client/src/components/footer/footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './footer.css'
+function FooterComp() {
+  return (
+    <div className="footer--div">&copy; Traffic offence manager- 2022</div>
+  )
+}
+
+export default FooterComp
---
+++ traffic_manager/client/src/isloggedin.js
@@ -0,0 +1,4 @@
+import { createContext } from "react"
+
+ const isLoggedin= createContext(null)
+ export default isLoggedin
