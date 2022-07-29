9a50774d589977a145f885f8af06a6ff14c6665b
S Sanjith <sanji123444@gmail.com>
2022-07-30 00:26:02 +0530

    Modified main.py

--- traffic_manager/main.py
+++ traffic_manager/main.py
@@ -2,24 +2,48 @@
 from flask import Flask, request, jsonify
 from flask_mysqldb import MySQL 
 
+
 #app config
 app=Flask(__name__)
 
 #DB config 
-app.config['MYSQL_HOST'] = ''
-app.config['MYSQL_USER'] = ''
-app.config['MYSQL_PASSWORD'] = ''
-app.config['MYSQL_DB'] = ''
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'your_password'
+app.config['MYSQL_DB'] = 'your_database'
   
   #SAMPLE MYSQL QUERY SNIPPET
 #    cursor = mysql.connection.cursor()
         # cursor.execute(''' INSERT INTO info_table VALUES(%s,%s)''',(name,age))
         # mysql.connection.commit()
 
+mysql = MySQL(app)
+
 #API endpoints
 @app.route('/')
 def index():
     return jsonify('hello world')
+
+@app.route('/offences/insert/<id>/<type>/<fine>')
+def offIns(id, type, fine):
+    cursor = mysql.connection.cursor()
+    cursor.execute(''' INSERT INTO offences VALUES(%s,%s,%s)''',(id,type,fine))
+    mysql.connection.commit()
+    return jsonify("Inserted!")
+
+@app.route('/offences/all')
+def offences():
+    cursor = mysql.connection.cursor()
+    cursor.execute(''' SELECT * FROM offences''')
+    details = cursor.fetchall()
+    return jsonify(details)
+
+@app.route('/offences/del/<id>')
+def offDel(id):
+    cursor = mysql.connection.cursor()
+    cursor.execute(''' DELETE FROM offences WHERE offenceid=%s''',(id))
+    mysql.connection.commit()
+    return jsonify("Deleted!")
 
 #app run config
 app.run(host='localhost', port= 5000)
